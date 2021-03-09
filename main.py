cbbce5d3019ad48aca9685a86c021a6b4c1093ee
Umair Asghar <56846748+UmairMughal901@users.noreply.github.com>
2021-03-09 13:04:16 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+# https://www.kaggle.com/kingabzpro/story-of-gufhtugu-publications
+import seaborn as sns
+import numpy as numpy
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import plotly as pl
+import lightgbm as lgb
+print("Importyrf all required libs")
